3418f11c1215f367a3244c88802564a2f189a075
israthnr <israthnr.20@uom.lk>
2024-01-07 21:31:45 +0530

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,16 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="nav">
+      <div className="nav-logo">Car Site</div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className="nav-contact">Contact</li>
+      </ul>
+    </div>
+
   )
 }
 
